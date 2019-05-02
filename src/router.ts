1d2fafa1858c28976ef60abf2b8906186443572e
fuyutarow <fuyutarow@gmail.com>
2019-05-03 05:17:57 +0900

    Add views/Welcome

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import { Home } from './views';
 
 Vue.use(Router);
 
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -14,12 +15,9 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/welcome',
+      name: 'welcome',
+      component: () => import(/* webpackChunkName: "about" */ './views/Welcome.vue'),
     },
   ],
 });
---
+++ src/views/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="Welcome">
+    <div class="headline">
+      Welcome to arXimdes
+    </div>
+    <v-btn to="/">get started</v-btn>
+  </div>
+</template>
